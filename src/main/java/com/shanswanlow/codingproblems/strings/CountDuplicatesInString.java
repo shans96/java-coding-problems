cab67912dd7f3ee4a859fafa70ab9cdba9dab4cf
shans96 <shanswanlow96@gmail.com>
2020-07-28 22:45:42 +0200

    Refactor method according to solution

--- src/main/java/com/shanswanlow/codingproblems/strings/CountDuplicatesInString.java
+++ src/main/java/com/shanswanlow/codingproblems/strings/CountDuplicatesInString.java
@@ -1,6 +1,7 @@
 package main.java.com.shanswanlow.codingproblems.strings;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CountDuplicatesInString
 {
@@ -11,26 +12,16 @@ public class CountDuplicatesInString
 
     private static int countDuplicates(String s)
     {
-        HashMap<Character, Integer> stringChars = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < s.length(); i++)
-        {
-            char current = s.charAt(i);
-            if (stringChars.get(current) != null)
-            {
-                stringChars.put(current, stringChars.get(current) + 1);
-            }
-            else
-            {
-                stringChars.put(current, 1);
-            }
-        }
+        Map<Character, Long> stringChars = s
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
-        for (int value : stringChars.values())
+        int count = 0;
+        for (Long value : stringChars.values())
         {
             count += value - 1;
         }
-
         return count;
     }
 }
