4469234c2b6bab90316aa3c1c192f428c0775298
shans96 <shanswanlow96@gmail.com>
2020-07-30 14:42:17 +0200

    Fix formatting

--- src/main/java/com/shanswanlow/codingproblems/strings/JoinStringsWithDelimiter.java
+++ src/main/java/com/shanswanlow/codingproblems/strings/JoinStringsWithDelimiter.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 public class JoinStringsWithDelimiter
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println(joinWithDelimiter(" ", "foo", "bar"));
         System.out.println(joinWithDelimiter("\n", "baz", "quux", "quuux"));
     }
